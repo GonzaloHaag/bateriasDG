cb1c2be8baa9c5ea9bcc8a66dec253dc0c838090
GonzaloHaag <gonzalohaag13@gmail.com>
2023-12-20 22:48:37 -0300

    Removi la funcion del header cuando se hacia scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,6 @@ import { Container } from 'react-bootstrap';
 import { NavbarMobile,NavbarDesktop } from './index';
 
 export const Header = () => {
-    const [headerFijo,setHeaderFijo] = useState(false);
-     const functionScroll = () => {
-       if(window.scrollY > 0) {
-        console.log('scroll');
-        setHeaderFijo(true);
-       }
-       else{
-        console.log('No scroll');
-        setHeaderFijo(false)
-       }
-     }
-     window.addEventListener('scroll',functionScroll);
   return (
     <header className='min-h-[65px] sticky w-full bg-white z-10 flex items-center top-0'>
         <Container>
